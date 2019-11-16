220be70c09abfb34a86e85bd866dcd62a81f278b
beyazit <beyazit@mpi-sws.org>
2019-11-16 22:57:50 +0300

    bu arada bora ben

---
+++ configurationScripts/configExp1S.sh
@@ -0,0 +1,11 @@
+r1=$(getent ahosts "r1" | cut -d " " -f 1 | uniq)
+r2=$(getent ahosts "r2" | cut -d " " -f 1 | uniq)
+r3=$(getent ahosts "r3" | cut -d " " -f 1 | uniq)
+
+r1_adapter=$(ip route get $r1 | grep -Po '(?<=(dev )).*(?= src| proto)')
+r2_adapter=$(ip route get $r2 | grep -Po '(?<=(dev )).*(?= src| proto)')
+r3_adapter=$(ip route get $r3 | grep -Po '(?<=(dev )).*(?= src| proto)')
+
+sudo tc qdisc add dev $r1_adapter root netem delay 20ms 5ms distribution normal
+sudo tc qdisc add dev $r2_adapter root netem delay 20ms 5ms distribution normal
+sudo tc qdisc add dev $r3_adapter root netem delay 20ms 5ms distribution normal
