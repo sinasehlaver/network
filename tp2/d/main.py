1abfecc55f2f12ffa545488bbe2781e5d834f9d5
beyazit <beyazit@mpi-sws.org>
2019-12-15 14:46:46 +0300

    creating header and divinding file into chunks is done

--- tp2/d/main.py
+++ tp2/d/main.py
@@ -1,12 +1,14 @@
 import socket
 import threading
+import struct
 
 def server(src_ip, src_port, dst_ip, dst_port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((src_ip, src_port))
     while True:
         packet, _ = server_socket.recvfrom(1024)
-        print(packet)
+        (checksum, length, seq_n, ack_n), payload = struct.unpack("!HHHH", packet[:8]), packet[8:]
+        print(checksum, length, seq_n, ack_n, payload)
 
 def main():
     t_server = threading.Thread(target=server, args=("10.10.7.1", 8080, "10.10.7.2", 8080))
--- tp2/s/main.py
+++ tp2/s/main.py
@@ -1,22 +1,40 @@
 import socket
 import threading
+import struct
+import sys
 
-def checksum(payload):
+def calculate_checksum(payload):
     s = 0
-    for i in range(0, len(payload), 2):
-        s += (payload[i] << 8) + payload[i + 1]
+    for i in range(len(payload)):
+        s <<= 8
+        s += payload[i]
         s = (s & 0xffff) + (s >> 16)
     return ~s & 0xffff
 
+def create_packet(seq_n, ack_n, payload):
+    checksum = calculate_checksum(payload)
+    length = 8 + len(payload)
+    return struct.pack("!HHHH", checksum, length, seq_n, ack_n) + payload
 
-def client(src_ip, src_port, dst_ip, dst_port):
+def divide_into_chunks(file_name, payload_size):
+    f = open(file_name, "rb")
+    content = f.read()
+    f.close()
+    n = int(len(content)/payload_size) + 1
+    chunks = []
+    for i in range(1, n):
+        chunks.append(content[(i - 1)*payload_size:i*payload_size])
+    return chunks
+
+def client(src_ip, src_port, dst_ip, dst_port, file_name):
+    chunks = divide_into_chunks(file_name, 512)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.bind((src_ip, src_port))
-    dummy_packet = ".".encode()
-    client_socket.sendto(dummy_packet, (dst_ip, dst_port))
+    for i in range(len(chunks)):
+        client_socket.sendto(create_packet(i, 1, chunks[i]), (dst_ip, dst_port))
 
 def main():
-    t_client = threading.Thread(target=client, args=("10.10.3.1", 8080, "10.10.3.2", 8080))
+    t_client = threading.Thread(target=client, args=("10.10.3.1", 8080, "10.10.3.2", 8080, sys.argv[1]))
     t_client.start()
     t_client.join()
 
