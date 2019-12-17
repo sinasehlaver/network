376347b128640b8ea084f5799ac0f910e1934818
beyazit <beyazit@mpi-sws.org>
2019-12-18 01:02:36 +0300

    xd

--- tp2/d/main.py
+++ tp2/d/main.py
@@ -1,17 +1,46 @@
 import socket
 import threading
 import struct
+import sys
 
-def server(src_ip, src_port, dst_ip, dst_port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind((src_ip, src_port))
+def calculate_checksum(payload):
+    s = 0
+    for i in range(len(payload)):
+        s <<= 8
+        s += payload[i]
+        s = (s & 0xffff) + (s >> 16)
+    return ~s & 0xffff
+
+def create_packet(seq_n, ack_n, payload="".encode()):
+    checksum = calculate_checksum(payload)
+    length = 8 + len(payload)
+    return struct.pack("!HHHH", checksum, length, seq_n, ack_n) + payload
+
+def file_receiver(src_ip, src_port, dst_ip, dst_port, file_name):
+    byte_chunks = []
+    expected_seq_n = 1
+    last_ack_packet = create_packet(0, 0)
+    file_receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    file_receiver_socket.bind((src_ip, src_port))
+    ack_sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
-        packet, _ = server_socket.recvfrom(1024)
+        if expected_seq_n == 10241:
+            break
+        packet, _ = file_receiver_socket.recvfrom(1024)
         (checksum, length, seq_n, ack_n), payload = struct.unpack("!HHHH", packet[:8]), packet[8:]
-        print(checksum, length, seq_n, ack_n, payload)
+        if seq_n == expected_seq_n and checksum == calculate_checksum(payload):
+            byte_chunks.append(payload)
+            expected_seq_n += 1
+            last_ack_packet = create_packet(0, seq_n)
+            ack_sender_socket.sendto(last_ack_packet, (dst_ip, dst_port))
+        else:
+            ack_sender_socket.sendto(last_ack_packet, (dst_ip, dst_port))
+    f = open(file_name, "wb+")
+    f.writelines(byte_chunks)
+    f.close()
 
 def main():
-    t_server = threading.Thread(target=server, args=("10.10.7.1", 8080, "10.10.7.2", 8080))
+    t_server = threading.Thread(target=file_receiver, args=("10.10.7.1", 8080, "10.10.7.2", 8080, sys.argv[1]))
     t_server.daemon = True
     t_server.start()
     t_server.join()
--- tp2/s/main.py
+++ tp2/s/main.py
@@ -3,6 +3,13 @@ import threading
 import struct
 import sys
 
+window_size = 10
+base = 0
+next_packet = 0
+
+base_lock = threading.Lock()
+next_packet_lock = threading.Lock()
+
 def calculate_checksum(payload):
     s = 0
     for i in range(len(payload)):
@@ -11,30 +18,58 @@ def calculate_checksum(payload):
         s = (s & 0xffff) + (s >> 16)
     return ~s & 0xffff
 
-def create_packet(seq_n, ack_n, payload):
+def create_packet(seq_n, ack_n, payload="".encode()):
     checksum = calculate_checksum(payload)
     length = 8 + len(payload)
     return struct.pack("!HHHH", checksum, length, seq_n, ack_n) + payload
 
-def divide_into_chunks(file_name, payload_size):
-    f = open(file_name, "rb")
+def divide_into_byte_chunks(file_name, payload_size):
+    f = open(file_name, "r")
     content = f.read()
     f.close()
     n = int(len(content)/payload_size) + 1
-    chunks = []
+    byte_chunks = []
     for i in range(1, n):
-        chunks.append(content[(i - 1)*payload_size:i*payload_size])
-    return chunks
+        byte_chunks.append(content[(i - 1)*payload_size:i*payload_size].encode())
+    return byte_chunks
 
-def client(src_ip, src_port, dst_ip, dst_port, file_name):
-    chunks = divide_into_chunks(file_name, 512)
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.bind((src_ip, src_port))
-    for i in range(len(chunks)):
-        client_socket.sendto(create_packet(i, 1, chunks[i]), (dst_ip, dst_port))
+def file_sender(src_ip, src_port, dst_ip, dst_port, file_name):
+    global window_size, base, next_packet
+    byte_chunks = divide_into_byte_chunks(file_name, 512)
+    file_sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ack_receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ack_receiver_socket.bind((src_ip, src_port))
+    ack_receiver_socket.settimeout(1)
+    next_packet_lock.acquire()
+    while next_packet < len(byte_chunks):
+        next_packet_lock.release()
+        next_packet_lock.acquire()
+        base_lock.acquire()
+        if next_packet < base + window_size:
+            file_sender_socket.sendto(create_packet(next_packet + 1, 0, byte_chunks[next_packet]), (dst_ip, dst_port))
+            next_packet += 1
+            threading.Thread(target=ack_receiver, args=(ack_receiver_socket,)).start()
+        base_lock.release()
+        next_packet_lock.release()
+        next_packet_lock.acquire()
+    next_packet_lock.release()
+
+def ack_receiver(ack_receiver_socket):
+    global base, next_packet
+    try:
+        packet, _ = ack_receiver_socket.recvfrom(1024)
+        checksum, length, seq_n, ack_n = struct.unpack("!HHHH", packet[:8])
+        base_lock.acquire()
+        if ack_n > base:
+            base = ack_n
+        base_lock.release()
+    except socket.timeout:
+        next_packet_lock.acquire()
+        next_packet = base
+        next_packet_lock.release()
 
 def main():
-    t_client = threading.Thread(target=client, args=("10.10.3.1", 8080, "10.10.3.2", 8080, sys.argv[1]))
+    t_client = threading.Thread(target=file_sender, args=("10.10.3.1", 8080, "10.10.3.2", 8080, sys.argv[1]))
     t_client.start()
     t_client.join()
 
