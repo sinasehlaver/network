1344c263e0ebb2afe266cd5253fcec58270884df
beyazit <beyazit@mpi-sws.org>
2019-12-19 01:08:35 +0300

    ittirdim

--- tp2/d/main.py
+++ tp2/d/main.py
@@ -2,48 +2,72 @@ import socket
 import threading
 import struct
 import sys
+import hashlib
 
 payload_size = 512
 N = 5*1024*1024//payload_size
 
+done = False
+
 byte_chunks = [None] * N
 
 def calculate_checksum(payload):
-    s = 0
-    for i in range(len(payload)):
-        s <<= 8
-        s += payload[i]
-        s = (s & 0xffff) + (s >> 16)
-    return ~s & 0xffff
+    return hashlib.md5(payload).hexdigest().encode()
 
 def create_packet(seq_n, ack_n, payload="".encode()):
     checksum = calculate_checksum(payload)
-    length = 8 + len(payload)
-    return struct.pack("!HHHH", checksum, length, seq_n, ack_n) + payload
+    length = 38 + len(payload)
+    return struct.pack("!HHH", seq_n, ack_n, length) + checksum + payload
 
-def file_receiver(src_ip, src_port, dst_ip, dst_port, file_name):
-    global byte_chunks
-    last_ack_packet = create_packet(0, 0)
+def extract_packet(packet):
+    return (*struct.unpack("!HHH", packet[:6]), packet[6:38], packet[38:])
+
+def file_receiver(src_ip, src_port, dst_ip, dst_port):
+    global done, byte_chunks, byte_chunks_locks
     file_receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     file_receiver_socket.bind((src_ip, src_port))
     ack_sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while True:
+        if done:
+            ack_sender_socket.sendto(create_packet(0, 0), (dst_ip, dst_port))
+            return
         packet, _ = file_receiver_socket.recvfrom(1024)
-        (checksum, length, seq_n, ack_n), payload = struct.unpack("!HHHH", packet[:8]), packet[8:]
+        seq_n, ack_n, _, checksum, payload = extract_packet(packet)
         if seq_n == 0 and ack_n == 0:
-            break
+            done = True
+            ack_sender_socket.sendto(create_packet(0, 0), (dst_ip, dst_port))
+            return
         if checksum == calculate_checksum(payload):
             if byte_chunks[seq_n - 1] == None:
                 byte_chunks[seq_n - 1] = payload
             ack_sender_socket.sendto(create_packet(0, seq_n), (dst_ip, dst_port))
+
+def write_to_file(file_name):
+    global byte_chunks
     f = open(file_name, "wb+")
     f.writelines(byte_chunks)
     f.close()
 
+def exp1():
+    t_file_receiver = threading.Thread(target=file_receiver, args=("10.10.7.1", 8080, "10.10.7.2", 8080))
+    t_file_receiver.start()
+    t_file_receiver.join()
+    write_to_file("output1")
+
+def exp2():
+    global byte_chunks
+    t_file_receiver_r1 = threading.Thread(target=file_receiver, daemon=True, args=("10.10.4.2", 8080, "10.10.8.1", 8080))
+    t_file_receiver_r2 = threading.Thread(target=file_receiver, daemon=True, args=("10.10.5.2", 8080, "10.10.8.2", 8080))
+    t_file_receiver_r1.start()
+    t_file_receiver_r2.start()
+    while not done:
+        continue
+    write_to_file("output2")
+
 def main():
-    t_server = threading.Thread(target=file_receiver, args=("10.10.7.1", 8080, "10.10.7.2", 8080, sys.argv[1]))
-    t_server.daemon = True
-    t_server.start()
-    t_server.join()
+    if sys.argv[1] == "exp1":
+        exp1()
+    elif sys.argv[1] == "exp2":
+        exp2()
     
 main()
--- tp2/s/main.py
+++ tp2/s/main.py
@@ -3,6 +3,8 @@ import threading
 import struct
 import sys
 import time
+import hashlib
+import os
 
 payload_size = 512
 N = 5*1024*1024//payload_size
@@ -10,24 +12,26 @@ N = 5*1024*1024//payload_size
 base = 0
 window_size = 8
 
-is_acked = [False] * N
+is_acked = [False] * (N + 1)
 is_sent = [False] * N
-byte_chunks = []
+byte_chunks = ["".encode()] * (N + 1)
 
 timeout_value = 1
 
+consecutive_timeouts = 0
+consecutive_timeouts_lock = threading.Lock()
+consecutive_timeouts_threshold = N//10
+
 def calculate_checksum(payload):
-    s = 0
-    for i in range(len(payload)):
-        s <<= 8
-        s += payload[i]
-        s = (s & 0xffff) + (s >> 16)
-    return ~s & 0xffff
+    return hashlib.md5(payload).hexdigest().encode()
 
 def create_packet(seq_n, ack_n, payload="".encode()):
     checksum = calculate_checksum(payload)
-    length = 8 + len(payload)
-    return struct.pack("!HHHH", checksum, length, seq_n, ack_n) + payload
+    length = 38 + len(payload)
+    return struct.pack("!HHH", seq_n, ack_n, length) + checksum + payload
+
+def extract_packet(packet):
+    return (*struct.unpack("!HHH", packet[:6]), packet[6:38], packet[38:])
 
 def divide_into_byte_chunks(file_name):
     global payload_size, N, byte_chunks
@@ -35,13 +39,18 @@ def divide_into_byte_chunks(file_name):
     content = f.read()
     f.close()
     for i in range(N):
-        byte_chunks.append(content[i*payload_size : (i + 1)*payload_size])
+        byte_chunks[i] = content[i*payload_size : (i + 1)*payload_size]
 
-def file_sender(dst_ip, dst_port):
-    global N, base, window_size, is_acked, is_sent, byte_chunks
+def file_sender(src_ip, src_port, dst_ip, dst_port):
+    global N, base, window_size, is_acked, is_sent, byte_chunks, consecutive_timeouts, consecutive_timeouts_lock, consecutive_timeouts_threshold
+    threading.Thread(target=ack_receiver, daemon=True, args=(src_ip, src_port)).start()
     file_sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     reached_end = False
-    while not all(is_acked):
+    while not all(is_acked[:-1]):
+        consecutive_timeouts_lock.acquire()
+        if consecutive_timeouts > consecutive_timeouts_threshold:
+            return
+        consecutive_timeouts_lock.release()
         if not reached_end:
             for i in range(base, base + window_size):
                 if is_acked[i]:
@@ -52,9 +61,9 @@ def file_sender(dst_ip, dst_port):
                         break
         for i in range(base, base + window_size):
             if not is_sent[i]:
-                threading.Thread(target=sender, args=(file_sender_socket, dst_ip, dst_port, i)).start()
+                threading.Thread(target=sender, daemon=True, args=(file_sender_socket, dst_ip, dst_port, i)).start()
                 is_sent[i] = True
-    file_sender_socket.sendto(create_packet(0, 0), (dst_ip, dst_port))
+    sender(file_sender_socket, dst_ip, dst_port, -1)
 
 def ack_receiver(src_ip, src_port):
     global is_acked
@@ -62,22 +71,47 @@ def ack_receiver(src_ip, src_port):
     ack_receiver_socket.bind((src_ip, src_port))
     while not all(is_acked):
         packet, _ = ack_receiver_socket.recvfrom(1024)
-        _, _, _, ack_n = struct.unpack("!HHHH", packet[:8])
-        is_acked[ack_n - 1] = True
+        seq_n, ack_n, _, _, _ = extract_packet(packet)
+        if ack_n == 0:
+            is_acked = [True] * (N + 1)
+        else:
+            is_acked[ack_n - 1] = True
 
 def sender(file_sender_socket, dst_ip, dst_port, i):
-    global is_acked, byte_chunks, timeout_value
+    global is_acked, byte_chunks, timeout_value, consecutive_timeouts, consecutive_timeouts_lock
+    is_timedout = False
     while not is_acked[i]:
+        if is_timedout:
+            consecutive_timeouts_lock.acquire()
+            consecutive_timeouts += 1
+            consecutive_timeouts_lock.release()
         file_sender_socket.sendto(create_packet(i + 1, 0, byte_chunks[i]), (dst_ip, dst_port))
         time.sleep(timeout_value)
+        is_timedout = True
+    consecutive_timeouts_lock.acquire()
+    consecutive_timeouts = 0
+    consecutive_timeouts_lock.release()
+
+def exp1():
+    divide_into_byte_chunks("input1")
+    file_sender("10.10.3.1", 8080, "10.10.3.2", 8080)
+
+def exp2():
+    divide_into_byte_chunks("input2")
+    # fork it
+    pid = os.fork()
+    if pid == 0:
+        # child, r1
+        file_sender("10.10.1.1", 8080, "10.10.1.2", 8080)
+    else:
+        # parent, r2
+        file_sender("10.10.2.2", 8080, "10.10.2.1", 8080)
+        os.waitpid(pid)
 
 def main():
-    divide_into_byte_chunks(sys.argv[1])
-    t_file_sender = threading.Thread(target=file_sender, args=("10.10.3.2", 8080))
-    t_ack_receiver = threading.Thread(target=ack_receiver, args=("10.10.3.1", 8080))
-    t_file_sender.start()
-    t_ack_receiver.start()
-    t_file_sender.join()
-    t_ack_receiver.join()
+    if sys.argv[1] == "exp1":
+        exp1()
+    elif sys.argv[1] == "exp2":
+        exp2()
 
 main()
