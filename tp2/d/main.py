2a2d5c8370deeb41b0b86de1ea266fd6c881e66b
beyazit <beyazit@mpi-sws.org>
2019-12-22 23:33:29 +0300

    of

--- tp2/d/main.py
+++ tp2/d/main.py
@@ -3,15 +3,13 @@ import threading
 import struct
 import sys
 import hashlib
-import time
+
+dst_ips = {"10.10.3.2":"10.10.7.2", "10.10.1.2":"10.10.8.1", "10.10.2.1":"10.10.8.2"}
 
 payload_size = 512
 N = 5*1024*1024//payload_size
 
-done = False
-
-byte_chunks = [None] * N
-byte_chunks_locks = [threading.Lock()] * N
+byte_chunks = [None]*N
 
 def calculate_checksum(payload):
     return hashlib.md5(payload).hexdigest().encode()
@@ -24,52 +22,40 @@ def create_packet(seq_n, ack_n, payload="".encode()):
 def extract_packet(packet):
     return (*struct.unpack("!HHH", packet[:6]), packet[6:38], packet[38:])
 
-def file_receiver(src_ip, src_port, dst_ip, dst_port):
-    global done, byte_chunks, byte_chunks_locks
+def file_receiver():
+    global dst_ips, payload_size, N, byte_chunks
     file_receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    file_receiver_socket.bind((src_ip, src_port))
+    file_receiver_socket.bind(("0.0.0.0", 8080))
     ack_sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    while not done:
-        packet, _ = file_receiver_socket.recvfrom(1024)
+    while True:
+        packet, (ip, port) = file_receiver_socket.recvfrom(1024)
         seq_n, ack_n, _, checksum, payload = extract_packet(packet)
-        if seq_n == 0 and ack_n == 0:
-            done = True
-            ack_sender_socket.sendto(create_packet(0, 0), (dst_ip, dst_port))
+        if seq_n == 0:
+            ack_sender_socket.sendto(create_packet(0, 0), (dst_ips[ip], port))
             return
         if checksum == calculate_checksum(payload):
-            byte_chunks_locks[seq_n - 1].acquire()
             if byte_chunks[seq_n - 1] == None:
                 byte_chunks[seq_n - 1] = payload
-                print(src_ip)
-            byte_chunks_locks[seq_n - 1].release()
-            ack_sender_socket.sendto(create_packet(0, seq_n), (dst_ip, dst_port))
+            ack_sender_socket.sendto(create_packet(0, seq_n), (dst_ips[ip], port))
 
 def write_to_file(file_name):
-    global byte_chunks
+    global dst_ips, payload_size, N, byte_chunks
     f = open(file_name, "wb+")
     f.writelines(byte_chunks)
     f.close()
 
-def exp1():
-    t_file_receiver = threading.Thread(target=file_receiver, args=("10.10.7.1", 8080, "10.10.7.2", 8080))
-    t_file_receiver.start()
-    t_file_receiver.join()
-    write_to_file("output1")
-
-def exp2():
-    global byte_chunks
-    t_file_receiver_r1 = threading.Thread(target=file_receiver, daemon=True, args=("10.10.4.2", 8080, "10.10.8.1", 8080))
-    t_file_receiver_r2 = threading.Thread(target=file_receiver, daemon=True, args=("10.10.5.2", 8080, "10.10.8.2", 8080))
-    t_file_receiver_r1.start()
-    t_file_receiver_r2.start()
-    while not done:
-    	continue
-    write_to_file("output2")
-
-def main():
+def sample():
     if sys.argv[1] == "exp1":
-        exp1()
+        output_file_name = "output1"
     elif sys.argv[1] == "exp2":
-        exp2()
+        output_file_name = "output2"
+    else:
+        return
+    file_receiver()
+    write_to_file(output_file_name)
     
+def main():
+	while True:
+		sample()
+
 main()
--- tp2/s/main.py
+++ tp2/s/main.py
@@ -4,27 +4,20 @@ import struct
 import sys
 import time
 import hashlib
-import os
+import statistics as st
+import math
+
+if sys.argv[1] == "exp1":
+    dst_ips = ["10.10.3.2", "10.10.3.2"]
+elif sys.argv[1] == "exp2":
+    dst_ips = ["10.10.1.2", "10.10.2.1"]
+else:
+    sys.exit()
 
 payload_size = 512
 N = 5*1024*1024//payload_size
-
-base = 0
-window_size = 8
-
-is_acked = [False] * (N + 1)
-is_sent = [False] * N
-byte_chunks = ["".encode()] * (N + 1)
-
-timeout_interval = 0.05
-estimated_rtt = None
-dev_rtt = None
-timeout_interval_lock = threading.Lock()
-starts = [None] * (N + 1)
-
-consecutive_timeouts = 0
-consecutive_timeouts_lock = threading.Lock()
-consecutive_timeouts_threshold = N//10
+window_size = 32
+byte_chunks = ["".encode()]*(N + 1)
 
 def calculate_checksum(payload):
     return hashlib.md5(payload).hexdigest().encode()
@@ -45,17 +38,25 @@ def divide_into_byte_chunks(file_name):
     for i in range(N):
         byte_chunks[i] = content[i*payload_size : (i + 1)*payload_size]
 
-def file_sender(src_ip, src_port, dst_ip, dst_port):
-    global N, base, window_size, is_acked, is_sent, byte_chunks, consecutive_timeouts, consecutive_timeouts_lock, consecutive_timeouts_threshold
-    threading.Thread(target=ack_receiver, daemon=True, args=(src_ip, src_port)).start()
+def file_sender():
+    global dst_ips, payload_size, N, base, window_size, is_acked, is_sent, byte_chunks, timeout_interval, estimated_rtt, dev_rtt, timeout_interval, estimated_rtt, timeout_interval_lock, starts, is_timedout, is_timedout_lock, timeouts
     file_sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     reached_end = False
     file_transfer_time_start = time.time()
     while not all(is_acked[:-1]):
-        consecutive_timeouts_lock.acquire()
-        if consecutive_timeouts > consecutive_timeouts_threshold:
-            return
-        consecutive_timeouts_lock.release()
+        is_timedout_lock.acquire()
+        if is_timedout:
+            is_timedout = False
+            for i in timeouts:
+                key = i % 2
+                if dst_ips[key] != dst_ips[(key + 1) % 2]:
+                    dst_ips[key] = dst_ips[(key + 1) % 2]
+                    timeout_interval[key] = timeout_interval[(key + 1) % 2]
+                    estimated_rtt[key] = estimated_rtt[(key + 1) % 2]
+                    dev_rtt[key] = dev_rtt[(key + 1) % 2]
+                threading.Thread(target=sender, daemon=True, args=(file_sender_socket, i)).start()
+            timeouts.clear()
+        is_timedout_lock.release()
         if not reached_end:
             for i in range(base, base + window_size):
                 if is_acked[i]:
@@ -64,78 +65,97 @@ def file_sender(src_ip, src_port, dst_ip, dst_port):
                     else:
                         reached_end = True
                         break
+                else:
+                    break
         for i in range(base, base + window_size):
             if not is_sent[i]:
-                threading.Thread(target=sender, daemon=True, args=(file_sender_socket, dst_ip, dst_port, i)).start()
+                threading.Thread(target=sender, daemon=True, args=(file_sender_socket, i)).start()
                 is_sent[i] = True
-    if not is_acked[-1]:
-        file_transfer_time_end = time.time()
-        print(file_transfer_time_end - file_transfer_time_start)
-        sender(file_sender_socket, dst_ip, dst_port, -1)
+    file_transfer_time_end = time.time()
+    sender(file_sender_socket, -1)
+    return file_transfer_time_end - file_transfer_time_start
 
-def ack_receiver(src_ip, src_port):
-    global is_acked, timeout_interval, estimated_rtt, dev_rtt, timeout_interval_lock, starts
+def ack_receiver():
+    global dst_ips, payload_size, N, base, window_size, is_acked, is_sent, byte_chunks, timeout_interval, estimated_rtt, dev_rtt, timeout_interval, estimated_rtt, timeout_interval_lock, starts, is_timedout, is_timedout_lock, timeouts
     ack_receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    ack_receiver_socket.bind((src_ip, src_port))
+    ack_receiver_socket.bind(("0.0.0.0", 8080))
     while not all(is_acked):
         packet, _ = ack_receiver_socket.recvfrom(1024)
         end = time.time()
         seq_n, ack_n, _, _, _ = extract_packet(packet)
-        if ack_n == 0:
-            is_acked = [True] * (N + 1)
-            return
-        else:
-            is_acked[ack_n - 1] = True
-        sample_rtt = end - starts[ack_n - 1]
-        if estimated_rtt == None:
-            estimated_rtt = sample_rtt
-        estimated_rtt = 0.875*estimated_rtt + 0.125*sample_rtt
-        temp = abs(sample_rtt - estimated_rtt)
-        if dev_rtt == None:
-            dev_rtt = temp
-        dev_rtt = 0.75*dev_rtt + 0.25*abs(temp)
-        timeout_interval_lock.acquire()
-        timeout_interval = estimated_rtt + 4*dev_rtt
-        timeout_interval_lock.release()
+        i = ack_n - 1
+        is_acked[ack_n - 1] = True
+        key = i % 2 if dst_ips[0] != dst_ips[1] else 0
+        sample_rtt = end - starts[i]
+        if estimated_rtt[key] == None:
+            estimated_rtt[key] = sample_rtt
+        estimated_rtt[key] = 0.875*estimated_rtt[key] + 0.125*sample_rtt
+        temp = abs(sample_rtt - estimated_rtt[key])
+        if dev_rtt[key] == None:
+            dev_rtt[key] = temp
+        dev_rtt[key] = 0.75*dev_rtt[key] + 0.25*abs(temp)
+        timeout_interval_lock[key].acquire()
+        timeout_interval[key] = estimated_rtt[key] + 4*dev_rtt[key]
+        timeout_interval_lock[key].release()
 
-def sender(file_sender_socket, dst_ip, dst_port, i):
-    global is_acked, byte_chunks, consecutive_timeouts, consecutive_timeouts_lock, starts, timeout_interval, timeout_interval_lock
-    is_timedout = False
+def sender(file_sender_socket, i):
+    global dst_ips, payload_size, N, base, window_size, is_acked, is_sent, byte_chunks, timeout_interval, estimated_rtt, dev_rtt, timeout_interval, estimated_rtt, timeout_interval_lock, starts, is_timedout, is_timedout_lock, timeouts
+    send_count = 0
+    key = i % 2 if dst_ips[0] != dst_ips[1] else 0
+    f = False
     while not is_acked[i]:
-        if is_timedout:
-            consecutive_timeouts_lock.acquire()
-            consecutive_timeouts += 1
-            consecutive_timeouts_lock.release()
-        timeout_interval_lock.acquire()
-        timeout_value = timeout_interval
-        timeout_interval_lock.release()
+        is_timedout_lock.acquire()
+        if send_count >= 100:
+            is_timedout = True
+            timeouts.append(i)
+            f = True
+        is_timedout_lock.release()
+        if f:
+            return
+        timeout_interval_lock[key].acquire()
+        timeout_value = timeout_interval[key]
+        timeout_interval_lock[key].release()
         starts[i] = time.time()
-        file_sender_socket.sendto(create_packet(i + 1, 0, byte_chunks[i]), (dst_ip, dst_port))
+        file_sender_socket.sendto(create_packet(i + 1, 0, byte_chunks[i]), (dst_ips[key], 8080))
         time.sleep(timeout_value)
-        is_timedout = True
-    consecutive_timeouts_lock.acquire()
-    consecutive_timeouts = 0
-    consecutive_timeouts_lock.release()
+        send_count += 1
 
-def exp1():
-    divide_into_byte_chunks("input1")
-    file_sender("10.10.3.1", 8080, "10.10.3.2", 8080)
+def init():
+    global dst_ips, payload_size, N, base, window_size, is_acked, is_sent, byte_chunks, timeout_interval, estimated_rtt, dev_rtt, timeout_interval, estimated_rtt, timeout_interval_lock, starts, is_timedout, is_timedout_lock, timeouts
+    base = 0
+    is_acked = [False]*(N + 1)
+    is_sent = [False]*N
+    timeout_interval = [0.05, 0.05]
+    estimated_rtt = [None, None]
+    dev_rtt = [None, None]
+    timeout_interval_lock = [threading.Lock(), threading.Lock()]
+    starts = [None]*(N + 1)
+    is_timedout = False
+    is_timedout_lock = threading.Lock()
+    timeouts = []
 
-def exp2():
-    divide_into_byte_chunks("input2")
-    pid = os.fork()
-    if pid == 0:
-        # child, r1
-        file_sender("10.10.1.1", 8080, "10.10.1.2", 8080)
-    else:
-        # parent, r2
-        file_sender("10.10.2.2", 8080, "10.10.2.1", 8080)
-        os.waitpid(pid, 0)
+def sample():
+    init()
+    threading.Thread(target=ack_receiver, daemon=True).start()
+    return file_sender()
 
 def main():
     if sys.argv[1] == "exp1":
-        exp1()
+        divide_into_byte_chunks("input1")
     elif sys.argv[1] == "exp2":
-        exp2()
+        divide_into_byte_chunks("input2")
+    ftts = []
+    error = 1
+    ftts.append(sample())
+    print(len(ftts), error, ftts[-1])
+    while error >= 0.025:
+        ftts.append(sample())
+        error = 1.96*st.stdev(ftts)/math.sqrt(len(ftts))
+        print(len(ftts), error, ftts[-1])
+    print("size = ", len(ftts))
+    print("mean = ", st.mean(ftts))
+    print("standard deviation = ", st.stdev(ftts))
+    print("margin of error = ", error)
+    print("median = ", st.median(ftts))
 
 main()
