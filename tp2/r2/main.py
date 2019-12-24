ef6e82d6e6749499d8def7f6af8bbb8ed9df6688
beyazit <beyazit@mpi-sws.org>
2019-12-24 17:21:36 +0300

    sina cok kotu

--- tp2/r2/main.py
+++ tp2/r2/main.py
@@ -9,12 +9,8 @@ def server(src_ip, src_port, dst_ip, dst_port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
     server_socket.bind((src_ip, src_port))
     while True:
-        try:
-            server_socket.settimeout(20)
-            data, addr = server_socket.recvfrom(1024)
-            server_socket.sendto(data, (dst_ip, dst_port))
-        except socket.timeout:
-            return
+        data, addr = server_socket.recvfrom(1024)
+        server_socket.sendto(data, (dst_ip, dst_port))
 
 def main():
     """
